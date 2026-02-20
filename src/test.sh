aa62e7adc257ff4f58aeab44b6fec193d008c29f
ahmed-maalawi1 <ahmedmaalawy001101@gmail.com>
2026-02-20 16:49:42 +0200

    fix workflow issue

--- src/test.sh
+++ src/test.sh
@@ -7,7 +7,7 @@ EXPECTED="Hello, Test!"
 
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
-if [ "$OUTPUT" == "$EXPECTED"]; then
+if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo " Test Passed!"
 
     exit 0
